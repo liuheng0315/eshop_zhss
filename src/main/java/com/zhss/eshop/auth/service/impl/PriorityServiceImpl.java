7ff59fc7cba228c1c35a9de5a127a97f0e708d3b
liuheng <liuheng0315@gmail.com>
2021-08-03 11:23:43 +0800

    修改配置

--- src/main/java/com/zhss/eshop/auth/service/impl/PriorityServiceImpl.java
+++ src/main/java/com/zhss/eshop/auth/service/impl/PriorityServiceImpl.java
@@ -11,6 +11,7 @@ import com.zhss.eshop.auth.visitor.PriorityNodeRelateCheckVisitor;
 import com.zhss.eshop.auth.visitor.PriorityNodeRemoveVisitor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +22,7 @@ import java.util.List;
  * @author liuheng
  */
 @Slf4j
+@Service
 public class PriorityServiceImpl implements PriorityService {
     @Autowired
     private PriorityDAO priorityDAO;
--- src/main/java/com/zhss/eshop/comment/dao/impl/CommentPictureDAOImpl.java
+++ src/main/java/com/zhss/eshop/comment/dao/impl/CommentPictureDAOImpl.java
@@ -2,16 +2,19 @@ package com.zhss.eshop.comment.dao.impl;
 
 import com.zhss.eshop.comment.dao.CommentPictureDAO;
 import com.zhss.eshop.comment.domain.CommentPictureDO;
+import com.zhss.eshop.comment.mapper.CommentPictureMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author : liu heng
  * @description :
  * @date : 2021-05-10 19:24
  **/
+@Repository
 public class CommentPictureDAOImpl implements CommentPictureDAO {
     @Autowired
-    private CommentPictureDAO commentPictureDAO;
+    private CommentPictureMapper commentPictureMapper;
     /**
      * 新增评论晒图
      *
@@ -19,6 +22,6 @@ public class CommentPictureDAOImpl implements CommentPictureDAO {
      */
     @Override
     public void saveCommentPicture(CommentPictureDO commentPictureDO) {
-        commentPictureDAO.saveCommentPicture(commentPictureDO);
+        commentPictureMapper.saveCommentPicture(commentPictureDO);
     }
 }
